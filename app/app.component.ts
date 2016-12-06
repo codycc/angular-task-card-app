ed3b50f1effb2d835947cf4bf4495b75658fffdb
Cody Condon <codycondon50@gmail.com>
2016-12-06 18:01:26 -0500

    adding data to iterate through each component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Task } from './model/task';
 
 @Component({
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent { 
+    private tasks = [
+        new Task(
+            "buy a monkey",
+            false
+        ),
+        new Task(
+            "walk the monkey",
+            false
+        )
+    ]
+ }
